42380aceabfb7bdf9bef597a457dbec7e478e34c
OOO <102598292+Ikeepmyideas@users.noreply.github.com>
2024-02-23 10:36:15 +0100

    Update server.js

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,6 +1,10 @@
-const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 3000;
-const server = http.createServer(app);
+const bodyParser = require('body-parser');
 
-server.listen(port);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
