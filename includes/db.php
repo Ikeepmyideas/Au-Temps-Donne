2553aa7bff423f0411ed63a7b7d78bf0479fe5a4
khaoula <khaoulachetioui4@gmail.com>
2024-02-23 10:08:20 +0100

    new version

--- includes/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    $bdd = new PDO('mysql:host=localhost;dbname=au_temps_donne', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-} catch (Exception $e) {
-    die('Erreur PDO : ' . $e->getMessage());
-}
-?>
