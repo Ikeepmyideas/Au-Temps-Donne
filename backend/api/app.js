3600d4fde32a18ea67ca585584c08cd53a194ef3
OOO <102598292+Ikeepmyideas@users.noreply.github.com>
2024-02-23 10:35:13 +0100

    Update app.js

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const beneficiaryRoutes = require('./routes/beneficiaryRoutes');
+const defaultRoute = require('./routes/defaultRoutes');
+const corsMiddleware = require('./middleware/cors');
 const app = express();
 
-app.use((req,res,next)=>{
-    res.status(200).json({
-        message: 'It is working !'
-    });
-});
+module.exports = app;
+
+app.use(corsMiddleware);
+app.use(bodyParser.json());
+
+app.use('/api', beneficiaryRoutes);
+app.use('/', defaultRoute);
 
 module.exports = app;
