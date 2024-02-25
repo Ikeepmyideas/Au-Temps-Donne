effa0293ebfe3c8897ca04f7099a7d9ab0017be9
OOO <102598292+Ikeepmyideas@users.noreply.github.com>
2024-02-25 22:23:43 +0100

    Update beneficiaryRoutes.js

--- backend/api/routes/beneficiaryRoutes.js
+++ backend/api/routes/beneficiaryRoutes.js
@@ -5,6 +5,8 @@ const authMiddleware = require('../middleware/authentication');
 
 router.post('/signup', beneficiaryController.signup);
 router.post('/login', beneficiaryController.login);
+router.put('/users/:userId', beneficiaryController.updateUser);
+router.delete('/users/:userId', beneficiaryController.deleteUser);
 
 
 module.exports = router;
