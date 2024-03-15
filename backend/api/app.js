7d5b72f83835efaa15883e76a1a524dbb25e5910
OOO <102598292+Ikeepmyideas@users.noreply.github.com>
2024-03-15 15:13:49 +0100

    Update app.js

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); 
+const cookieParser = require('cookie-parser'); 
 const beneficiaryRoutes = require('./routes/beneficiaryRoutes');
 const defaultRoute = require('./routes/defaultRoutes');
 const corsMiddleware = require('./middleware/cors');
 const app = express();
+const corsOptions = {
+    origin: 'http://localhost',
+    credentials: true
+};
 
-module.exports = app;
-
+app.use(cors(corsOptions));
 app.use(corsMiddleware);
+app.use(cookieParser());
 app.use(bodyParser.json());
-
 app.use('/api', beneficiaryRoutes);
 app.use('/', defaultRoute);
 
