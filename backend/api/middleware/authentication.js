002a3f8475d7cef62a0acb353f995421efbc851a
OOO <102598292+Ikeepmyideas@users.noreply.github.com>
2024-03-15 15:06:33 +0100

    Update authentication.js

--- backend/api/middleware/authentication.js
+++ backend/api/middleware/authentication.js
@@ -1,21 +1,21 @@
 const jwt = require('jsonwebtoken');
 
-function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    
-    if (!authHeader) {
-        return res.sendStatus(401); 
-    }
+const authMiddleware = (req, res, next) => {
+  const token = req.cookies.access_token;
 
-    const token = authHeader.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized: No token provided' });
+  }
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) {
-            return res.sendStatus(403); 
-        }
-        req.user = user; 
-        next(); 
-    });
-}
+  try {
+    const decoded = jwt.verify(token, 'your-secret-key');
+    req.user = decoded;
+    next();
+  } 
+  catch (error) {
+    console.error(error);
+    return res.status(401).json({ error: 'Unauthorized: Invalid token' });
+  }
+};
 
-module.exports = authenticateToken;
+module.exports = authMiddleware;
