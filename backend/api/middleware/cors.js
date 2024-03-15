299276990c8162883fe4623d20274d330306b327
OOO <102598292+Ikeepmyideas@users.noreply.github.com>
2024-03-15 15:10:02 +0100

    Update cors.js

--- backend/api/middleware/cors.js
+++ backend/api/middleware/cors.js
@@ -1,7 +1,11 @@
-
 module.exports = function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(204);
+    } else {
+        next();
+    }
 };
